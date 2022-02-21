f6e2f0dbf5b7a271e02fca12f4f122db1c602a1d
Abhinav Yadav <mr.kumarabhinayadav@gmail.com>
2022-02-22 00:39:31 +0530

    form input bug

--- src/components/Inputs.tsx
+++ src/components/Inputs.tsx
@@ -1,18 +1,38 @@
+import { useState } from "react";
+
 export default function Input(){
+   const selectedLanguage  = 'python';
+   const codeSnippit= {
+ 'python':"print('Hello World')"
+   };
+   const handelSubmission= (event:any) =>{
+       event.preventDefault();
+       console.log(`New data ${sourceCode}`);
+
+   }
+    const [sourceCode,setSourcecode]  = useState(codeSnippit.python);
     return (
-      
+    
       <div >
+  <form onSubmit={handelSubmission}  
+  onChange={(e) => e.target}  
+  >
 
           <div className="col-sm-6"> 
 
-      <textarea name="" id="" cols={30} rows={10}></textarea>
+      <textarea name="" id="" cols={30} rows={10}
+      value={sourceCode}
+      
+      ></textarea>
           </div>
 
           <div>
               <div className="btn btn-primary">Compile</div>
           </div>
+      </form>
 
       </div>
 
+    
     );
 } 
